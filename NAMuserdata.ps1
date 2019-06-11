919a7dea56e382f0898f2b4a52a3de072a3d2896
bsipswitch <37195381+bsipswitch@users.noreply.github.com>
2019-06-11 10:16:50 -0500

    Update NAMuserdata.ps1

--- NAMuserdata.ps1
+++ NAMuserdata.ps1
@@ -1,2 +1,2 @@
 wget https://repo.saltstack.com/windows/Salt-Minion-2019.2.0-Py3-x86-Setup.exe -OutFile c:\salt.exe
-iex 'C:\salt.exe /S /master=salt.nam.local /minion-name=$env:computername'
+iex 'C:\salt.exe /S /master=10.44.2.4 /minion-name=$env:computername'
